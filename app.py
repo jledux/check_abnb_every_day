7720a2e17c25a219ae33b27c215f593054c74399
Julien SAMSON <julien.samson@gmail.com>
2020-09-12 17:08:04 +0100

    Ajout des différentes étapes au code.

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-"""Check every day, if you have a new arrivall on airbnb.com and send the information to someone via email."""
+"""
+Check every day, if you have a new arrivall on airbnb.com and
+send the information to someone via email.
+"""
+
+# TODO: Récupérer les identifianf de airbnb et de l'email à qui envoyer
+
+# TODO: Se conneter au compte Airbnb
+
+# TODO: Vérifier s'il y a un nouvel évènement
